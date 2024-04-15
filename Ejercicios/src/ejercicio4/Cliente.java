337a9b65e626265cd8c14ca911c000bd7dff290f
Joselu549 <joseluisabellanmonreal@gmail.com>
2024-04-15 17:21:00 +0200

    Ejercicio 4 TERMINADO

---
+++ Ejercicios/src/ejercicio4/Cliente.java
@@ -0,0 +1,56 @@
+package ejercicio4;
+
+import java.util.Random;
+
+import messagepassing.*;
+
+public class Cliente implements Runnable {
+	private CommunicationScheme[] cajas = new MailBox[2];
+	private int id;
+	private String caja = "A";
+	
+	public Cliente(CommunicationScheme[] cajas, int id) {
+		this.cajas = cajas;
+		this.id = id;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			/* 1.- Realiza la compra */
+			Random r = new Random();
+			try {
+				Thread.sleep(500 + r.nextInt(700 - 500));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			int tiempopago = 1 + r.nextInt(10 - 1);
+			/* 2.- Solicita entrar a la caja que le corresponde */
+			if (tiempopago >= 5) {
+				/* Seleccionar caja A */
+				Object llaveA = cajas[0].receive();
+				try {
+					Thread.sleep(tiempopago * 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				cajas[0].send(llaveA);
+			} else {
+				/* Seleccionar caja B */
+				Object llaveB = cajas[1].receive();
+				caja = "B";
+				try {
+					Thread.sleep(tiempopago * 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				cajas[1].send(llaveB);
+			}
+			System.out.println("--------------------------------------------------------------");
+			System.out.println("Persona " + id + " ha usado la caja " + caja);
+			System.out.println("Tiempo de pago = " + tiempopago + "s");
+			System.out.println("Persona " + id + " liberando la caja " + caja);
+			System.out.println("--------------------------------------------------------------");
+		}
+	}
+}
--- Ejercicios/src/ejercicio4/Main.java
+++ Ejercicios/src/ejercicio4/Main.java
@@ -1,10 +1,28 @@
 package ejercicio4;
 
+import messagepassing.MailBox;
+
 public class Main {
-
+	public static int MAX_CLIENTES = 30;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MailBox[] cajas = new MailBox[2];
+		Thread[] clientes = new Thread[MAX_CLIENTES];
+		cajas[0] = new MailBox();
+		cajas[0].send("llaveA");
+		cajas[1] = new MailBox();
+		cajas[1].send("llaveB");
+		for (int i = 0; i < MAX_CLIENTES; i++) {
+			clientes[i] = new Thread(new Cliente(cajas, i));
+			clientes[i].start();
+		}
+		
+		try {
+			for (int i = 0; i < MAX_CLIENTES; i++) {
+				clientes[i].join();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Termina el programa");
 	}
-
 }
